dc089c4432e8723b4ae20f31d2f23ced01e2d58b
Kat <kyevsukov@gmail.com>
2021-10-04 17:56:16 -0400

    number click handlers set up

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  const handleAddOne = () => {
-    dispatch(addOne())
+  // const handleAddOne = () => {
+  //   dispatch(addOne())
+  // }
+
+  const handleNumberClick = (number) => {
+    dispatch(applyNumber(number))
   }
 
   return (
@@ -38,21 +42,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={() => {handleAddOne()}}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => {handleNumberClick(1)}}/>
+              <CalcButton value={2} onClick={() => {handleNumberClick(2)}}/>
+              <CalcButton value={3} onClick={() => {handleNumberClick(3)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => {handleNumberClick(4)}}/>
+              <CalcButton value={5} onClick={() => {handleNumberClick(5)}}/>
+              <CalcButton value={6} onClick={() => {handleNumberClick(6)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => {handleNumberClick(7)}}/>
+              <CalcButton value={8} onClick={() => {handleNumberClick(8)}}/>
+              <CalcButton value={9} onClick={() => {handleNumberClick(9)}}/>
             </div>
 
             <div className="row">
